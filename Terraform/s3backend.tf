d1b673f01ccc39bbc31be0fe5f0df8a317359122
vivekrajdev <37079673+vivekrajdev@users.noreply.github.com>
2023-12-17 15:53:11 +0530

    json to use s3 as backend

--- Terraform/s3backend.tf
+++ Terraform/s3backend.tf
@@ -1,1 +1,16 @@
-
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+  backend "s3" {
+    bucket = "terraform-071"
+    key    = "terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+provider "aws" {
+  region = "us-east-1"
+}
